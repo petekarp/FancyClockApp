2461cee47d8a5cd32f21a3f6d4f60517fb51f37e
OJ7 <omairj7@gmail.com>
2014-11-13 22:00:39 -0500

    Fixed updating settings :+1:

--- app/src/main/java/net/karp/javed/fancyclockapp/MainActivity.java
+++ app/src/main/java/net/karp/javed/fancyclockapp/MainActivity.java
@@ -3,8 +3,10 @@ package net.karp.javed.fancyclockapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -20,15 +22,13 @@ public class MainActivity extends Activity {
     SimpleDateFormat timeFormat, dateFormat;
     boolean militaryTime;
     String clockColor, clockSize;
-
     final String MILITARY_TIME = "military_time",
             CLOCK_COLOR = "clock_color",
             CLOCK_SIZE = "clock_size";
-
     String timeString, dateString;
-
-
     SharedPreferences prefs;
+    TextView timeView,dateView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +37,9 @@ public class MainActivity extends Activity {
         setTitle(R.string.title_simple_clock);
         prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         setContentView(R.layout.activity_main);
+
+        timeView = (TextView) findViewById(R.id.time_view);
+        dateView = (TextView) findViewById(R.id.date_view);
 
         updateStuff();
         updateClock();
@@ -54,6 +57,7 @@ public class MainActivity extends Activity {
         clockColor = prefs.getString(CLOCK_COLOR, getResources().getStringArray(R.array.colors_array)[0]);
         clockSize = prefs.getString(CLOCK_SIZE, getResources().getStringArray(R.array.size_array)[0]);
 
+        // Setting 24 Hour time setting
         if(militaryTime)
         {
             timeString = "HH:mm:ss";
@@ -64,8 +68,41 @@ public class MainActivity extends Activity {
         }
         dateString = "MMMM dd, yyyy";
 
-        //updateClock(time, date);
-        //updateClock();
+        // Setting Text Color
+        if(clockColor.equals(getResources().getString(R.string.black)))
+        {
+            timeView.setTextColor(Color.BLACK);
+        }
+        else if(clockColor.equals(getResources().getString(R.string.blue)))
+        {
+            timeView.setTextColor(Color.BLUE);
+        }
+        else if(clockColor.equals(getResources().getString(R.string.green)))
+        {
+            timeView.setTextColor(Color.GREEN);
+        }
+        else if(clockColor.equals(getResources().getString(R.string.red)))
+        {
+            timeView.setTextColor(Color.RED);
+        }
+
+        // Setting Text Size
+        if(clockSize.equals(getResources().getString(R.string.small)))
+        {
+            timeView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+        }
+        else if(clockSize.equals(getResources().getString(R.string.medium)))
+        {
+            timeView.setTextSize(TypedValue.COMPLEX_UNIT_SP,22);
+        }
+        else if(clockSize.equals(getResources().getString(R.string.large)))
+        {
+            timeView.setTextSize(TypedValue.COMPLEX_UNIT_SP,30);
+        }
+        else if(clockSize.equals(getResources().getString(R.string.extra_large)))
+        {
+            timeView.setTextSize(TypedValue.COMPLEX_UNIT_SP,50);
+        }
     }
 
     //public void updateClock(final String timeString, final String dateString){
@@ -88,8 +125,6 @@ public class MainActivity extends Activity {
                                 time = timeFormat.format(c);
                                 dateFormat = new SimpleDateFormat(dateString);
                                 date = dateFormat.format(c);
-                                TextView timeView = (TextView) findViewById(R.id.time_view);
-                                TextView dateView = (TextView) findViewById(R.id.date_view);
                                 timeView.setText(time);
                                 dateView.setText(date);
                             }
